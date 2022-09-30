5b3570f5e2edbc3e501699391529bc43e4767123
ShlingBop <Dominicyun1@hotmail.com>
2022-09-30 18:02:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'birthName' : 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation' :  'London, United Kingdom',
+        'age' : 29
+    },
+    'chance the rapper': {
+        'birthName' : 'Chancelor Johnathan Bennett',
+        'birthLocation' :  'Chicago, United States',
+        'age' : 29
+    },
+    'unknown': {
+        'birthName' : 'unknown',
+        'birthLocation' :  'unknown',
+        'age' : 'unknown'
+    }
+    
+}
+
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+ 
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Better Go Catch It!!!`)
+})
